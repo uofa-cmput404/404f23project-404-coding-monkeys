981626b687bd9c6d9184d35358be23a242072d6f
apollo-online <alynch1@ualberta.ca>
2023-10-17 00:12:18 -0600

    list users page w/ hyperlinks, individual user pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 # DFB pg. 61
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, ListProfilesView, AuthorDetailView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"), # display HomePageView
+    path("service/authors", ListProfilesView.as_view(), name="authors_list"), # display list of users on server; is this the exact url they want?
+    path("service/authors/<int:pk>", AuthorDetailView.as_view(), name="author_profile"), # display author's profile
 ]
