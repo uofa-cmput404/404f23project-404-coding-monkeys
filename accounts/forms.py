2d0623b2188a61d1411d0ce0bdae5c7129ecf4db
apollo-online <alynch1@ualberta.ca>
2023-10-18 00:18:06 -0600

    users can edit their profiles information, set a custom profile image

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,20 @@
 # DFB pg. 167
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import AuthorUser
+from django.forms import ModelForm
 
-class AuthorCreationForm(UserCreationForm):
+
+class AuthorCreationForm(UserCreationForm): # choose which fields should be displayed in sign-up form (password requested by default)
     class Meta(UserCreationForm):
         model = AuthorUser
-        fields = ("username", "email", "github", "profile_image") # choose which fields should be displayed in sign-up form (password requested by default)
+        fields = ("username", "email", "github", "profile_image") 
 
-class AuthorChangeForm(UserChangeForm):
+class AuthorChangeForm(UserChangeForm): # used to edit author profiles in django admin view; I don't think it can be accessed by the user
     class Meta:
         model = AuthorUser
         fields = UserChangeForm.Meta.fields
+
+class AuthorUpdateForm(ModelForm): # used to allow authors to edit their own profile
+    class Meta:
+        model = AuthorUser
+        fields = ["username", "email", "github", "profile_image"]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,19 @@
 # DFB pg. 177
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
+from accounts.models import AuthorUser
 from .forms import AuthorCreationForm
+from accounts.forms import AuthorUpdateForm
 
 class SignUpView(CreateView):
     form_class = AuthorCreationForm
     success_url = reverse_lazy('login')
     template_name = "registration/signup.html"
+
+class AuthorUpdateView(UpdateView): # gpt
+    model = AuthorUser
+    form_class = AuthorUpdateForm
+    template_name = 'editprofile.html'
+    
+    def get_success_url(self): # gpt
+        return reverse_lazy('author_profile', kwargs={'pk': self.object.pk})
