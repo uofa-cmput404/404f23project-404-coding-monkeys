75fa1d1b7f6a1cd01594603de89db5606bf42a76
apollo-online <alynch1@ualberta.ca>
2023-10-23 22:53:35 -0600

    fix

--- pages/urls.py
+++ pages/urls.py
@@ -6,13 +6,14 @@ from . import views  # need this for follow to work
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"), # display HomePageView
-    path("service/authors/", ListProfilesView.as_view(), name="authors_list"), # display list of users on server; is this the exact url they want?
-    path("service/authors/<int:pk>/", AuthorDetailView.as_view(), name="author_profile"), # display author's profile
-    path("service/authors/<int:pk>/editprofile/", AuthorUpdateView.as_view(), name="author_edit"), # edit user's profile
-    path("service/authors/<int:pk>/followed/", views.follow_author, name="author_followed"), 
+    path("authors/", ListProfilesView.as_view(), name="authors_list"), # display list of users on server; is this the exact url they want?
+    path("authors/<int:pk>/", AuthorDetailView.as_view(), name="author_profile"), # display author's profile
+    path("authors/<int:pk>/editprofile/", AuthorUpdateView.as_view(), name="author_edit"), # edit user's profile
+    path("authors/<int:pk>/followed/", views.follow_author, name="author_followed"), 
     path("service/authors/<int:pk>/followrequests/", FollowRequestsListView.as_view(), name="author_requests"), 
     path("service/authors/<int:pk>/followrequests/accept/<int:fq_pk>", views.accept_fq, name="fq_accept"),
     path("service/authors/<int:pk>/followrequests/deny/<int:fq_pk>", views.deny_fq, name="fq_deny"),
+    path("authors/my_profile", views.view_my_profile, name="my_profile"), # get all authors on server
 ]
 
 #TODO are the urls supposed to have a terminating /? Because when I try service/authors/ it gives me a 404. Adding terminating / for now.
