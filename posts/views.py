6cba98fde9d2ef7a5ef7421091dc2e40589ab35c
Sophia <97320246+sruduke@users.noreply.github.com>
2023-10-30 00:42:59 -0600

    commit before push refactor

--- posts/views.py
+++ posts/views.py
@@ -1,27 +1,31 @@
 from django.shortcuts import render, get_object_or_404, redirect 
-from django.views.generic import DetailView, CreateView
+from django.views.generic import CreateView
 from django.forms.models import model_to_dict
 from .models import Posts, Likes
-from .forms import CreatePostForm
-from django.http import HttpResponseRedirect
+from .forms import PostForm
 from django.urls import reverse
-from accounts.models import AuthorUser
+from accounts.models import AuthorUser, Followers
 import uuid
 import base64
-from django.http import HttpResponse
 from django.http import JsonResponse
+from django.core.files.base import ContentFile
+from PIL import Image
 
 class PostCreate(CreateView):
     model = Posts
     template_name = "posts/create.html"
-    form_class = CreatePostForm
+    form_class = PostForm
 
     def get_success_url(self): # gpt
         return reverse('all')
 
     def form_valid(self, form):
         # TODO: add author and full link to unique ID, this is merely the hash
-        unique_id = uuid.uuid4()
+        print(form.cleaned_data)
+        unique_id = form.cleaned_data.get('uuid')
+        if unique_id in (None, ''):
+            unique_id = uuid.uuid4()
+        unique_id_pic = str(unique_id) + "_pic"
 
         title = form.cleaned_data.get('title')
         description = form.cleaned_data.get('description')
@@ -34,46 +38,181 @@ class PostCreate(CreateView):
         
         if any(empty):
             return super().form_invalid(form)
-        else:
-            contentType = "text/plain"
-            author = get_author_info(self.request) # convert author object to dictionary
-            count = 0
-            comments = ""
+        
+        contentType = "text/plain"
+        author = get_author_info(self.request) # convert author object to dictionary
+        count = 0
+        comments = ""
 
-            if picture is not None:
-                image_base64 = base64.b64encode(picture.read()).decode('utf-8')
-                unique_id_pic = str(unique_id) + "_pic"
+        if picture is not None:
+            #save the pic as it's own post
+            image_base64, contentType_pic = get_picture_info(picture)
+            post_pic = Posts(title=title, uuid=unique_id_pic, description=description, contentType=contentType_pic, content=image_base64, categories=categories, visibility=visibility, author=author, count=count, comments=comments, unlisted=True)
+            post_pic.save(force_insert=True)
 
-                #Determine content type:
-                if picture.name.endswith(".jpg") or picture.name.endswith(".jpeg"):
-                    contentType_pic = "image/jpeg;base64"
-                elif picture.name.endswith(".png"):
-                    contentType_pic = "image/png;base64"
-                else:
-                    contentType_pic = "application/base64"
-
-                #save the pic as it's own post
-                post_pic = Posts(title=title, uuid=unique_id_pic, description=description, contentType=contentType_pic, content=image_base64, categories=categories, visibility=visibility, author=author, count=count, comments=comments, unlisted=True)
-                post_pic.save(force_insert=True)
-
-            post = Posts(title=title, uuid=unique_id, description=description, contentType=contentType, content=content, categories=categories, visibility=visibility, author=author, count=count, comments=comments, unlisted=False)
-            post.save(force_insert=True)
+        post = Posts(title=title, uuid=unique_id, description=description, contentType=contentType, content=content, categories=categories, visibility=visibility, author=author, count=count, comments=comments, unlisted=False)
+        post.save(force_insert=True)
             
+        return redirect('stream')
+
+def make_new_post(request, form=None):
+    if request.method == 'GET':
+        try:
+            authors = AuthorUser.objects.get_queryset()
+        except AuthorUser.DoesNotExist:
+            authors = []
+        
+        if form:
+            return render(request, 'posts/create.html', {'form': form, 'author_list':authors})
+        else:
+            return render(request, 'posts/create.html', {'form': PostForm(), 'author_list':authors})
+    elif request.method == 'POST':
+        return update_or_create_post(request, None)
+        
+
+def get_picture_info(picture):
+    image_base64 = base64.b64encode(picture.read()).decode('utf-8')
+
+    #Determine content type:
+    if picture.name.endswith(".jpg") or picture.name.endswith(".jpeg"):
+        contentType_pic = "image/jpeg;base64"
+    elif picture.name.endswith(".png"):
+        contentType_pic = "image/png;base64"
+    else:
+        contentType_pic = "application/base64"
+    
+    return image_base64, contentType_pic
+
+def update_or_create_post(request, post_uuid):
+    newPost = False
+    # create or grab post first
+    try:
+        if post_uuid:
+            post = Posts.objects.get(uuid=post_uuid)
+            unique_id = post_uuid
+        else:
+            raise Posts.DoesNotExist()
+    except Posts.DoesNotExist:
+        newPost = True
+        unique_id = uuid.uuid4()
+        author = get_author_info(request) # convert author object to dictionary
+        post = Posts(uuid=unique_id, author=author, contentType="text/plain", count=0, comments="", unlisted=False)
+
+    unique_id_pic = str(unique_id) + "_pic"
+
+    post.title = request.POST.get('title')
+    post.description = request.POST.get('description')
+    post.content = request.POST.get('content')
+    post.categories = request.POST.get('categories')
+    post.visibility = request.POST.get('visibility')
+    post.sharedWith = request.POST.get('sharedWith')
+
+    post.save(force_insert=True)
+    
+    # deal with updating picture
+    try:
+        post = Posts.objects.get(uuid=unique_id_pic)
+    except Posts.DoesNotExist:
+        post.uuid = unique_id_pic
+        post.unlisted = True
+
+    # update existing pic post
+    if request.FILES.get('picture') is not None or not newPost:
+        image_base64, contentType_pic = get_picture_info(request.FILES.get('picture'))
+        post.content = image_base64
+        post.contentType = contentType_pic
+        post.save(force_insert=True)
+
+    return redirect('stream')
+
+
+def edit_post(request, author_id, post_uuid):
+    if request.method == 'GET':
+        #read the post (whose like button the user clicked) object from db
+        try:
+            post = Posts.objects.get(uuid=post_uuid)
+
+            # try:
+            #     pic_post = Posts.objects.get(uuid=f'{post_uuid}_pic')
+            #     image_data = base64.b64decode(pic_post.content)
+            #     image_file = ContentFile(image_data)
+            #     image = Image.open(image_file)
+            # except Posts.DoesNotExist:
+            #     image = None
+
+            form_data = {
+                'uuid': post_uuid,
+                'title': post.title,
+                'description': post.description,
+                'categories': post.categories,
+                'content': post.content,
+                'visibility': post.visibility,
+                'picture': ''
+            }
+
+            return make_new_post(request, PostForm(initial=form_data))
+        except Posts.DoesNotExist: 
+            return redirect('stream')
+        
+    elif request.method == 'POST':
+        # gather picture info
+        if request.FILES.get('picture') is not None:
+            image_base64, contentType_pic = get_picture_info(request.FILES.get('picture'))
+
+        # update mere post first
+        try:
+            post = Posts.objects.get(uuid=post_uuid)
+
+            post.title = request.POST.get('title')
+            post.description = request.POST.get('description')
+            post.content = request.POST.get('content')
+            post.categories = request.POST.get('categories')
+            post.visibility = request.POST.get('visibility')
+
+            post.save()
+        except Posts.DoesNotExist:
+            # TODO raise error popup
             return redirect('stream')
 
-def edit_post(request):
-    post_uuid = request.GET.get('post_uuid', None) #get the post in question
-    author = get_author_info(request) #get the current user
+        # deal with updating picture
+        try:
+            unique_id_pic = str(post_uuid) + "_pic"
+            post_pic = Posts.objects.get(uuid=unique_id_pic)
 
-    #read the post (whose like button the user clicked) object from db
-    try:
-        post = Posts.objects.get(uuid=post_uuid)
-        return render(request, 'posts/create.html', model_to_dict(post))
-    except Posts.DoesNotExist: 
-        print(f"Error: Post with UUID:{post_uuid} does not exist.")
-    
-    
+            # update existing pic post
+            # TODO might need a case to delete the pic post if the user deletes the picture
+            if request.FILES.get('picture') is not None:
+                post_pic.content = image_base64
+                post_pic.contentType = contentType_pic
+                post_pic.save(force_insert=True)
+            else:
+                post_pic.delete()
 
+        except Posts.DoesNotExist:
+            # otherwise make a new pic post if a picture was added
+            if request.FILES.get('picture') is not None:
+                # copy all fields related to post but change UUID and picture relevant fields
+                post.uuid = unique_id_pic
+                post.unlisted = True
+                post.contentType = contentType_pic
+                post.content = image_base64
+                post.save(force_insert=True)
+
+        return redirect('stream')
+
+def delete_post(request, author_id, post_uuid):
+    if request.method == 'GET':
+        #read the post (whose like button the user clicked) object from db
+        try:
+            post = Posts.objects.get(uuid=post_uuid)
+            post.delete()
+
+            pic_post = Posts.objects.get(uuid=f'{post_uuid}_pic')
+            pic_post.delete()
+        except Posts.DoesNotExist: 
+            return redirect('stream')
+        
+        return redirect('stream')
 
 def get_author_info(request):
     user = request.user # get db information of current user
@@ -90,9 +229,49 @@ def get_author_info(request):
 
     return clean_dict
 
+
+def determine_if_friends(current_followers : list, user_id : str, post_author_id : str):
+    try:
+        row = Followers.objects.get(author_id=post_author_id)
+        author_followers = [follower['id'] for follower in row.followers]
+    except Followers.DoesNotExist:
+        author_followers = []
+    
+    return post_author_id in current_followers and user_id in author_followers
+
+
 def view_posts(request):
+    author_id = request.user.id
+    viewable = []
+    determined_friends = set()
+    # exclude picture posts since they're dealt with in the html template
     posts = Posts.objects.order_by('-published')
-    return render(request, 'posts/dashboard.html', {'all_posts': posts})
+
+    try:
+        row = Followers.objects.get(author_id=author_id)
+        current_followers = [follower['id'] for follower in row.followers]
+    except Followers.DoesNotExist:
+        current_followers = []
+
+    for post in posts:
+        if post.visibility == "PUBLIC":
+            viewable.append(post)
+
+        # show own posts
+        elif post.author['id'] == author_id:
+            viewable.append(post)
+
+        elif post.visibility == "FRIENDS":
+            if post.author['id'] in determined_friends or determine_if_friends(current_followers, author_id, post.author['id']):
+                viewable.append(post)
+                determined_friends.add(post.author['id'])
+
+        elif post.visibility == "PRIVATE":
+            sharedWith = post.sharedWith.get('id', None)
+            if sharedWith == author_id:
+                viewable.append(post)
+
+    return render(request, 'posts/dashboard.html', {'all_posts': viewable})
 
 def like_post_handler(request):
     #The user has clicked the like button for a post.
