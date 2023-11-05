d6f5f3dc1df7847ea760b6a9a7acb2169d9bc12e
Sophia Ruduke <sruduke@ualberta.ca>
2023-11-05 15:44:18 -0700

    comments

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from accounts.models import Posts
 from pages.seralizers import AuthorSerializer
 
+# started but needs to be finished
 class PostsSerializer(serializers.ModelSerializer):
     
     type = serializers.CharField(default="posts", max_length=6)
