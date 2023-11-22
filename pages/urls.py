8775f7eb90895fcb6194025ecde16b5220c2b39b
Sophia Ruduke <sruduke@ualberta.ca>
2023-11-21 22:55:31 -0700

    endpoints for liked

--- pages/urls.py
+++ pages/urls.py
@@ -33,6 +33,13 @@ urlpatterns = [
     # Comment API Calls
     path('authors/<str:uuid>/posts/<str:post_id>/comments/', post_views.api_comments, name='api_comments'),
 
+    # Likes API Calls
+    path('authors/<str:uuid>/posts/<str:post_id>/likes/', post_views.api_likes, name='api_post_likes'),
+    path('authors/<str:uuid>/posts/<str:post_id>/comments<str:comment_id>', post_views.api_comments, name='api_comment_likes'),
+
+    # Public Liked API Calls
+    path('authors/<str:uuid>/liked/', post_views.api_liked, name='api_author_liked'),
+
     # Local paths
     # ======================================================================================================================================
 
