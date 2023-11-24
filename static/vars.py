98a2302b9c511b771f85b626c0546ba4861fecaf
Matthew Wood <mwood2@cs.ualberta.ca>
2023-11-24 16:49:21 -0700

    Changed the vars.py to use local endpoint

--- static/vars.py
+++ static/vars.py
@@ -1,5 +1,9 @@
 # this represents static variables that are used throughout various parts of the application
 
 # this is the DEV URL of our webserver; we'll need a PROD URL later when we actually deploy
-ENDPOINT = "http://www.chimp-chat.win/"
-HOSTS = ["http://www.chimp-chat.win"]
+
+# ENDPOINT = "http://www.chimp-chat.win/"
+# HOSTS = ["http://www.chimp-chat.win"]
+
+ENDPOINT = "http://127.0.0.1:8000/"
+HOSTS = ["http://127.0.0.1:8000", "http://www.chimp-chat.win"]
