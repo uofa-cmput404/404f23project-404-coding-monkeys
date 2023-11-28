f427400648dc1cdc788cee91046c3b548b8067ea
Matthew Wood <mwood2@cs.ualberta.ca>
2023-11-28 11:26:14 -0700

    Added files Soph said for running locally, crashing upon dashboard

---
+++ static/vars.py
@@ -0,0 +1,5 @@
+# this represents static variables that are used throughout various parts of the application
+
+# this is the DEV URL of our webserver; we'll need a PROD URL later when we actually deploy
+ENDPOINT = "http://localhost:8000/"
+HOSTS = ["http://localhost:8000"]
