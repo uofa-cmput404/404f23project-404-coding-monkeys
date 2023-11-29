94a817a3537c3515bba682412937ba5dd499fb69
NimaShariatz <77418451+NimaShariatz@users.noreply.github.com>
2023-11-29 04:29:55 -0700

    Delete static/vars.py

--- static/vars.py
+++
@@ -1,3 +0,0 @@
-# static vars used throughout project
-ENDPOINT = "http://localhost:8000/"
-HOSTS = ["http://localhost:8000", "https://distributed-network-37d054f03cf4.herokuapp.com", "https://chimp-chat-1e0cca1cc8ce.herokuapp.com"]
