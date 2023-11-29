3f484d303f42aecea2de338ae655a162f028ffba
Matthew Wood <mwood2@cs.ualberta.ca>
2023-11-29 11:50:36 -0700

    Fixed a db error that's new overnight, but now comments dont work at all

--- posts/views.py
+++ posts/views.py
@@ -288,7 +288,7 @@ def post_stream(request):
 
         if post["contentType"] == "text/markdown":
             post["content"] = commonmark.commonmark(post["content"])
-        elif post["origin"].startswith(HOSTS[1]) and post["contentType"] not in ("text/plain", "text/markdown"):
+        elif post["content"] and post["origin"].startswith(HOSTS[1]) and post["contentType"] not in ("text/plain", "text/markdown"):
             post["content"] = post["content"].split(",")[1] if len(post["content"].split(",")) == 2 else post["content"]
         toReturn.append(post)
     sorted_posts = sorted(toReturn, key=lambda x: x["published"], reverse=True)
