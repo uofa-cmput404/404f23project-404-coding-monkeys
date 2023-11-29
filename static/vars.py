adef36b0c0afbeef4f3a5f90e5b57ed5965a4639
NimaShariatz <shariatz@ualberta.ca>
2023-11-28 20:10:09 -0700

    changed header so that more zoom is allowed

---
+++ static/vars.py
@@ -0,0 +1,3 @@
+# static vars used throughout project
+ENDPOINT = "http://localhost:8000/"
+HOSTS = ["http://localhost:8000", "https://distributed-network-37d054f03cf4.herokuapp.com", "https://chimp-chat-1e0cca1cc8ce.herokuapp.com"]
