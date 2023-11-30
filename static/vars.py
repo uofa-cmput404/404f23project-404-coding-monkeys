dab83502eaa854b6f85d83bfb63da23a431da21c
Sophia Ruduke <sruduke@ualberta.ca>
2023-11-29 22:02:09 -0700

    config files

---
+++ static/vars.py
@@ -0,0 +1,3 @@
+# static vars used throughout project
+ENDPOINT = "https://chimp-chat-1e0cca1cc8ce.herokuapp.com/"
+HOSTS = ["https://chimp-chat-1e0cca1cc8ce.herokuapp.com", "https://distributed-network-37d054f03cf4.herokuapp.com"]
