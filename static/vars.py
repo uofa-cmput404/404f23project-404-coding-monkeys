bd4e6e076c6157472a38463fee9fa72f911bdc71
Sophia Ruduke <sruduke@ualberta.ca>
2023-12-03 20:52:12 -0700

    added a-team as connection

--- static/vars.py
+++ static/vars.py
@@ -6,4 +6,6 @@ HOSTS = ["http://localhost:8000",
         # Web Wizards
         "https://webwizards-backend-952a98ea6ec2.herokuapp.com/service",
         # Ctrl-alt-dft
-        "https://cmput404-ctrl-alt-defeat-api-12dfa609f364.herokuapp.com"]
+        "https://cmput404-ctrl-alt-defeat-api-12dfa609f364.herokuapp.com",
+        # A-Team
+        "https://c404-5f70eb0b3255.herokuapp.com"]
