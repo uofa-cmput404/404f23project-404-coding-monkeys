653a72b464b6b134c6bdc0b412cebb3c557011b5
Sophia Ruduke <sruduke@ualberta.ca>
2023-12-03 21:16:36 -0700

    localhost

--- static/vars.py
+++ static/vars.py
@@ -1,11 +1,3 @@
 # static vars used throughout project
 ENDPOINT = "http://localhost:8000/"
-HOSTS = ["http://localhost:8000", 
-        # 404 Not Found
-        "https://distributed-network-37d054f03cf4.herokuapp.com", 
-        # Web Wizards
-        "https://webwizards-backend-952a98ea6ec2.herokuapp.com/service",
-        # Ctrl-alt-dft
-        "https://cmput404-ctrl-alt-defeat-api-12dfa609f364.herokuapp.com",
-        # A-Team
-        "https://c404-5f70eb0b3255.herokuapp.com"]
+HOSTS = ["http://localhost:8000"]
