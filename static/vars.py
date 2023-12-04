41811d5ff360cae3a1e800a60d1b881f7dcc5132
Sophia Ruduke <sruduke@ualberta.ca>
2023-12-04 16:46:12 -0700

    i swear if this works

---
+++ static/vars.py
@@ -0,0 +1,11 @@
+# static vars used throughout project
+ENDPOINT = "https://chimp-chat-1e0cca1cc8ce.herokuapp.com/"
+HOSTS = ["https://chimp-chat-1e0cca1cc8ce.herokuapp.com", 
+        # 404 Not Found
+        "https://distributed-network-37d054f03cf4.herokuapp.com", 
+        # Web Wizards
+        "https://webwizards-backend-952a98ea6ec2.herokuapp.com/service",
+        # Ctrl-alt-dft
+        "https://cmput404-ctrl-alt-defeat-api-12dfa609f364.herokuapp.com",
+        # A-Team
+        "https://c404-5f70eb0b3255.herokuapp.com"]
