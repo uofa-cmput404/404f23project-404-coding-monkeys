aabb884116300d510dbd9141e5396dceae36397d
apollo-online <alynch1@ualberta.ca>
2023-12-04 20:46:24 -0700

    can discern refresh

--- connections/views.py
+++ connections/views.py
@@ -8,6 +8,7 @@ from drf_yasg import openapi
 from .models import Node
 from cryptography.fernet import Fernet
 import django_project.settings as settings
+from connections.caches import AuthorCache, PostCache
 # Create your views here.
 
 @swagger_auto_schema(methods=['GET'], auto_schema=None)
@@ -22,3 +23,17 @@ def docs_viewer(request):
 
 def extra_docs_viewer(request):
     return render(request, "remote_docs.html")
+
+@api_view(['GET'])
+def refreshCache(request):
+    print("---REFRESH TRIGGERED---")
+
+    t = request.GET.get('type')
+
+    if t == "posts":
+        post_cache = PostCache()
+        post_cache.update()
+    elif t == "authors":
+        author_cache = AuthorCache()
+        author_cache.update()
+    return Response(status=200, data={"message": "Cache updated"})
