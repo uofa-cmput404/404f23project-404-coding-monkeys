6ce4b2230f277e8d748e627fc45ee894dacc3ddb
Sophia Ruduke <sruduke@ualberta.ca>
2023-12-06 01:39:46 -0700

    troubleshooting

--- connections/caches.py
+++ connections/caches.py
@@ -235,7 +235,7 @@ class PostCache(Cache):
         for author, details in node_authors:
             try:
                 # skip local posts
-                if strip_slash(details['host']) != node_index:
+                if strip_slash(details['host']) != HOSTS[node_index]:
                     continue
                 
                 index = HOSTS.index(strip_slash(details['host']))
